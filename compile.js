d197f9f047621d08d47496ceb8e9729d867e004f
Murphy Hsieh <murphyhsieh@leandev.io>
2020-04-14 10:48:57 +0800

    deploying first inbox contract, interface and bytecode are important

--- compile.js
+++ compile.js
@@ -18,4 +18,4 @@ const source = fs.readFileSync(inboxPath, 'utf8');
     }
  * ***/
 
-module.exports = solc.compile(source, 1);
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,8 +2,11 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
    // Get a list of all accounts
@@ -12,10 +15,13 @@ beforeEach(async () => {
 
    // Use one of those accounts to deploy
    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
     it('deploy a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     })
 });
