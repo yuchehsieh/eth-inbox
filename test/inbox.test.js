fad354a497cf8c42c5629cd1e22929fad55c4d0f
Murphy Hsieh <murphyhsieh@leandev.io>
2020-04-14 10:06:16 +0800

    first testing using mocha

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,29 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car {
+    part() {
+        return 'stopped'
+    }
+
+    drive() {
+        return 'vroom'
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can stop', () => {
+        assert.equal(car.part(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
