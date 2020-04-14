c88df675db08afd4dea202a9ed6d5787067b26ad
Murphy Hsieh <murphyhsieh@leandev.io>
2020-04-14 17:39:16 +0800

    test message() and setMessage()

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,12 +1,16 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
 
+/** UPDATE TO THIS because of the web3 library get updated! **/
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there';
 
 beforeEach(async () => {
    // Get a list of all accounts
@@ -16,13 +20,26 @@ beforeEach(async () => {
    // Use one of those accounts to deploy
    // the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .deploy({data: bytecode, arguments: [INITIAL_STRING]})
         .send({from: accounts[0], gas: '1000000'});
+
+    /** ADD THIS LINE because of the web3 library get updated! **/
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploy a contract', () => {
-        console.log(inbox);
+        // console.log(inbox);
         assert.ok(inbox.options.address); /*** .ok means that the value exist or truthy ***/
-    })
+    });
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+    it('can update the message', async () => {
+        const tx = await inbox.methods.setMessage('Hello world').send({from: accounts[0], gas: '1000000'});
+        // console.log(tx);
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hello world');
+    });
 });
